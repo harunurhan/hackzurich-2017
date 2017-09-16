b2003498c2a4acc6d033aff1a87ecfd52591e8d5
Roman <rom-meier@hotmail.com>
2017-09-16 18:59:34 +0200

    merged f und b

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 
 // serve frontend app
 var app = express();
-app.use(express.static('../frontend'));
+// app.use(express.static('../frontend'));
 
 // endpoint to latest news example: /news?last=10m
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -21,16 +21,16 @@ app.post('/luis', (req, res) => {
         .then(result => res.json(result));
 })
 
-app.listen(8080, () => {
-  console.log('Serving static files at port 8080');
-});
+// app.listen(8080, () => {
+//   console.log('Serving static files at port 8080');
+// });
 
 // serve socket
 const socket = Socket({
   path: '',
   serveClient: false,
 });
-socket.attach(3000, {
+socket.attach(8080, {
   pingInterval: 10000,
   pingTimeout: 5000,
   cookie: false
